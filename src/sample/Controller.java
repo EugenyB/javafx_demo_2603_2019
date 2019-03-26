8eecd4873959589fb77b260f2a4cb791ba08aeb5
EugenyB <eberkunsky@gmail.com>
2019-03-26 22:40:32 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+
+import java.util.List;
+
+public class Controller {
+    @FXML
+    private ListView<Student> list;
+
+    @FXML
+    public void initialize() {
+        List<Student> students = List.of(
+                new Student(1, "Ivanov", 25),
+                new Student(3, "Petov", 19),
+                new Student(4, "Kotovsky", 20)
+        );
+        list.setItems(FXCollections.observableList(students));
+    }
+
+    public void about() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Наша мега крутая программа");
+        alert.setTitle("About");
+        alert.setHeaderText(null);
+        alert.show();
+    }
+
+    public void close() {
+        Platform.exit();
+    }
+}
