4e622f2d21677dfe43385652658508b8719be867
EugenyB <eberkunsky@gmail.com>
2019-04-02 10:58:26 +0300

    changed directory structure

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,10 +6,17 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.List;
 
 public class Controller {
+    @FXML private TableView<Student> table;
+    @FXML private TableColumn<Student,Integer> idColumn;
+    @FXML private TableColumn<Student, String> nameColumn;
+    @FXML private TableColumn<Student, Integer> ageColumn;
     @FXML
     private ListView<Student> list;
 
@@ -18,9 +25,27 @@ public class Controller {
         List<Student> students = List.of(
                 new Student(1, "Ivanov", 25),
                 new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
+                new Student(3, "Petov", 19),
                 new Student(4, "Kotovsky", 20)
         );
         list.setItems(FXCollections.observableList(students));
+
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        ageColumn.setCellValueFactory(new PropertyValueFactory<>("age"));
+        table.setItems(FXCollections.observableList(students));
     }
 
     public void about() {
